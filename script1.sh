3b62feb46ef370db0090751e3f42bae849f7ec62
Nasr <Norwani@smail.pembinatrails.ca>
2024-10-24 15:10:26 -0500

    Fixed Error in script

--- script1.sh
+++ script1.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-echo "Welcome to my script, would you like to make a new directory or monitor your systems health?"
-read -p "For making a new directory type '1'. For monitoring your system health type '2'." user_option
+echo "Welcome to my script, would you like to make a new directory or monitor your system's health?"
+read -p "For making a new directory type '1'. For monitoring your system health type '2': " user_option
 
-if ["$user_option" = "1"] then
-	read -p "Enter directory name:" user_input
+if [ "$user_option" = "1" ]; then
+  read -p "Enter directory name: " user_input
 
+  if [ -d "$user_input" ]; then
+    echo "Directory '$user_input' already exists."
+  else
+    echo "Directory '$user_input' does not exist. Creating now."
+    mkdir "$user_input"
+    echo "Directory '$user_input' created."
+  fi
 
-if [ -d "$user_input" ]; then
-  echo "Directory '$user_input' already exists."
+elif [ "$user_option" = "2" ]; then
+  echo "Opening the system monitoring app..."
+  gnome-system-monitor
+
 else
-  echo "Directory '$user_input' does not exist. Creating now."
-  mkdir "$user_input"
-  echo "Directory '$user_input' created."
+  echo "Invalid option selected."
 fi
-elif ["$user_option" = "2" ]; then
-	echo "Opening the system monitoring app..."
-	gnome-system-monitor
